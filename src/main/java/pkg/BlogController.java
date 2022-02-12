f7dc4bb72310b8b73a87b3f7fb7a68402780e9c4
Tacanga <juan.tacanga@accenture.com>
2022-02-11 18:36:01 -0800

    back to Controller

--- src/main/java/pkg/BlogController.java
+++ src/main/java/pkg/BlogController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class BlogController {
     
     private PostRepository postRepository;
